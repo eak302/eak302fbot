2304739e7b87e7bdf526bafb1238d7849e01141c
eak302 <eak302@gmail.com>
2016-11-23 14:59:48 +0700

    modify index.php

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 	//echo "hello";
     //EAAX7OvvVP7kBAMomgnF8pds9MngEv8y9kxBWEFFBD21I4Q8QUS6DKu89jZAR8xJa24w47Qz1rZAjp1EvGFHeNVWgCLMAJ0qfmqpR4EwNxOe2HrCPPK7Wq76NDaNOI5KjDIJSSdpEKKw4jVlY2ZAFIRAGgcZAoEHGzL2pAXHBJwZDZD
 
+    //curl -ik -X POST "https://graph.facebook.com/v2.6/me/subscribed_apps?access_token=EAAX7OvvVP7kBAMomgnF8pds9MngEv8y9kxBWEFFBD21I4Q8QUS6DKu89jZAR8xJa24w47Qz1rZAjp1EvGFHeNVWgCLMAJ0qfmqpR4EwNxOe2HrCPPK7Wq76NDaNOI5KjDIJSSdpEKKw4jVlY2ZAFIRAGgcZAoEHGzL2pAXHBJwZDZD"
+
     // parameters
     $hubVerifyToken = 'TOKEN123456abcd';
     $accessToken = "EAAX7OvvVP7kBAMomgnF8pds9MngEv8y9kxBWEFFBD21I4Q8QUS6DKu89jZAR8xJa24w47Qz1rZAjp1EvGFHeNVWgCLMAJ0qfmqpR4EwNxOe2HrCPPK7Wq76NDaNOI5KjDIJSSdpEKKw4jVlY2ZAFIRAGgcZAoEHGzL2pAXHBJwZDZD";
@@ -15,17 +17,29 @@
     $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
     $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
     $answer = "I don't understand. Ask me 'hi'.";
-    if($messageText == "hi") {
-        $answer = "Hello";
+
+    if ($messageText != "")
+    {
+        if($messageText == "hi")
+        {
+            $answer = "Hello";
+        }
+        else
+        {
+
+        }
+        $response = [
+            'recipient' => [ 'id' => $senderId ],
+            'message' => [ 'text' => $answer ]
+        ];
+        $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_exec($ch);
+        curl_close($ch);
+        //based on http://stackoverflow.com/questions/36803518
+        
     }
-    $response = [
-        'recipient' => [ 'id' => $senderId ],
-        'message' => [ 'text' => $answer ]
-    ];
-    $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-    curl_exec($ch);
-    curl_close($ch);
-    //based on http://stackoverflow.com/questions/36803518
+
+    
