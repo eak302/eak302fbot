e890c38d5741806755a0cc4a4a1260cfa251c356
eak302 <eak302@gmail.com>
2016-11-23 14:05:30 +0700

    edit index.php file

--- index.php
+++ index.php
@@ -2,8 +2,30 @@
 	//echo "hello";
     //EAAX7OvvVP7kBAMomgnF8pds9MngEv8y9kxBWEFFBD21I4Q8QUS6DKu89jZAR8xJa24w47Qz1rZAjp1EvGFHeNVWgCLMAJ0qfmqpR4EwNxOe2HrCPPK7Wq76NDaNOI5KjDIJSSdpEKKw4jVlY2ZAFIRAGgcZAoEHGzL2pAXHBJwZDZD
 
-    $verify_token = "EAAX7OvvVP7kBAMomgnF8pds9MngEv8y9kxBWEFFBD21I4Q8QUS6DKu89jZAR8xJa24w47Qz1rZAjp1EvGFHeNVWgCLMAJ0qfmqpR4EwNxOe2HrCPPK7Wq76NDaNOI5KjDIJSSdpEKKw4jVlY2ZAFIRAGgcZAoEHGzL2pAXHBJwZDZD";
-
-    if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) { 
-        echo $_REQUEST['hub_challenge']; 
+    // parameters
+    $hubVerifyToken = 'TOKEN123456abcd';
+    $accessToken = "EAAX7OvvVP7kBAMomgnF8pds9MngEv8y9kxBWEFFBD21I4Q8QUS6DKu89jZAR8xJa24w47Qz1rZAjp1EvGFHeNVWgCLMAJ0qfmqpR4EwNxOe2HrCPPK7Wq76NDaNOI5KjDIJSSdpEKKw4jVlY2ZAFIRAGgcZAoEHGzL2pAXHBJwZDZD";
+    // check token at setup
+    if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+      echo $_REQUEST['hub_challenge'];
+      exit;
     }
+    // handle bot's anwser
+    $input = json_decode(file_get_contents('php://input'), true);
+    $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+    $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+    $answer = "I don't understand. Ask me 'hi'.";
+    if($messageText == "hi") {
+        $answer = "Hello";
+    }
+    $response = [
+        'recipient' => [ 'id' => $senderId ],
+        'message' => [ 'text' => $answer ]
+    ];
+    $ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token='.$accessToken);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_exec($ch);
+    curl_close($ch);
+    //based on http://stackoverflow.com/questions/36803518
